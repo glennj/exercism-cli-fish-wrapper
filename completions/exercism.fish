94f5177217b7c3af8c528252b6b2fbd5651cbe06
Glenn Jackman <glenn.jackman@gmail.com>
2022-07-17 16:15:20 -0400

    Commentary on extra logic in completion

--- completions/exercism.fish
+++ completions/exercism.fish
@@ -1,14 +1,18 @@
 # *Append* my new subcommands to an existing completion
 
-# return immediately if we've sourced this file already
+# Return immediately if we've sourced this file already
 set -q __EXERCISM__ADDITIONAL_COMPLETIONS; and return
 set -g __EXERCISM__ADDITIONAL_COMPLETIONS yes
 
+# Find the "official" exercism completions in the complete path.
+# This loop will find this script as well, which is why
+# the guard above exists (to avoid infinite sourcing).
 for dir in $fish_complete_path
     test -f "$dir/exercism.fish"
     and source "$dir/exercism.fish"
 end
 
+# Now, add the additional subcommands for completion
 complete -f -c exercism -n "__fish_use_subcommand" -a "refresh" -d "Re-downloads a submitted exercise"
 complete -f -c exercism -n "__fish_use_subcommand" -a "test-all" -d "Refresh and retest every exercise for a track"
 complete -f -c exercism -n "__fish_use_subcommand" -a "cleanup" -d "Clean up build artifacts (some tracks)"
