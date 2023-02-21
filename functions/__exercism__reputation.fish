91e1d408bce6fe0159421c8fded9dca9bfd63462
Glenn Jackman <glenn.jackman@gmail.com>
2023-02-21 10:50:35 -0500

    quiet reputation

--- functions/__exercism__reputation.fish
+++ functions/__exercism__reputation.fish
@@ -4,12 +4,13 @@ function __exercism__reputation
 Show recent reputation from exercism.
 
 Options
-    --all   Show last 10 reputation awards.
-            The default is to show only unseen ones.
-    --mark  Mark all unseen reputation as seen.'
+    --all       Show last 10 reputation awards.
+                The default is to show only unseen ones.
+    --mark      Mark all unseen reputation as seen.
+    -q|--quiet  Don\'t show any output. Use with --mark.'
 
     argparse --name="exercism reputation" \
-        'h/help' 'all' 'mark' -- $argv
+        'h/help' 'all' 'mark' 'q/quiet' -- $argv
     or return 1
 
     if set -q _flag_help
@@ -22,28 +23,33 @@ Options
         set _flag_all yes
     end
 
-    set results (
-        __exercism__api_call reputation \
-        | jq -L (realpath (status dirname)/../lib) \
-             --arg all "$_flag_all" \
-             -r '
-                  include "duration";
-                  .results
-                  | map(if $all == "" then select(.is_seen == false) else . end)
-                  | .[:10][]
-                  | [
-                      (.created_at | fromdateiso8601 | duration),
-                      .value,
-                      if .is_seen then "" else "*" end,
-                      (.text | gsub("</?strong>"; ""))
-                    ]
-                  | @csv
-                '
-    )
-    if test (count $results) -eq 0
-        echo "No unseen reputation"
-    else
-        string join \n $results \
-        | mlr --c2p --barred --implicit-csv-header label When,Amount,S,What
+    if not set -q _flag_quiet
+        set json (__exercism__api_call reputation)
+        set results (
+            echo $json \
+            | jq -L (realpath (status dirname)/../lib) \
+                --arg all "$_flag_all" \
+                -r '
+                      include "duration";
+                      .results
+                      | map(if $all == "" then select(.is_seen == false) else . end)
+                      | .[:10][]
+                      | [
+                          (.created_at | fromdateiso8601 | duration),
+                          .value,
+                          if .is_seen then "" else "*" end,
+                          (.text | gsub("</?strong>"; ""))
+                        ]
+                      | @csv
+                    '
+        )
+        if test (count $results) -eq 0
+            echo "No unseen reputation"
+        else
+            string join \n $results \
+            | mlr --c2p --barred --implicit-csv-header label When,Amount,S,What
+        end
+        echo
+        echo $json | jq -r '"Your total reputation: \(.meta.total_reputation)"'
     end
 end
