d8473a65a9e706e374206e29186805cdd9fe1988
Glenn Jackman <jackman@pythian.com>
2024-11-26 15:54:27 -0500

    x

--- functions/__exercism__check__perl5.fish
+++ functions/__exercism__check__perl5.fish
@@ -2,13 +2,12 @@ function __exercism__check__perl5
     argparse --ignore-unknown t/track= -- $argv
 
     set solution (jq -r '.files.solution[]' .exercism/config.json)
+    set files (find (dirname $solution[1]) -name \*.pm -print)
 
     if not type -q perltidy
         echo "perltidy not found: cpanm Perl::Tidy" >&2
     else
-        for file in $solution
-            __echo_and_execute perltidy -b -bext='/' $file
-        end
+        __echo_and_execute perltidy -b -bext='/' $files
     end
 
     echo
