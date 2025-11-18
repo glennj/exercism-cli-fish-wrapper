a0231535605ab7359b07e96bb0fd1d1c01f8caac
Glenn Jackman <glenn.jackman@gmail.com>
2025-11-18 14:01:51 -0500

    tweak stats

--- functions/__exercism__stats.fish
+++ functions/__exercism__stats.fish
@@ -6,9 +6,10 @@ Gather some statistics about Exercism exercises.
 Options
     -d|--download
             Download all the track config.json files into the
-            XDG_CACHE_HOME directory.'
+            XDG_CACHE_HOME directory.
+    -a|--all    Include exercises that only appear on one track.'
 
-    argparse --name="exercism data" 'h/help' 'd/download' -- $argv
+    argparse --name="exercism data" 'h/help' 'd/download' 'a/all' -- $argv
     or return 1
 
     if set -q _flag_help
@@ -21,8 +22,11 @@ Options
         set cache_dir $HOME/.cache
     end
 
-    if set -q _flag_download
+    if not test -d $cache_dir/exercism/tracks
+        set _flag_download true
+    end
 
+    if set -q _flag_download
         __exercism__api_call '/tracks' \
         | jq -r '.tracks[].slug' \
         | while read slug
@@ -33,9 +37,11 @@ Options
             curl --output $track_dir/config.json --silent --location $url
         end
         echo
+        date > $cache_dir/exercism/tracks/latest_download
     end
 
     pushd $cache_dir/exercism/tracks
+
     jq -r '
         .exercises.practice[]
         | select(.status != "deprecated")
@@ -46,6 +52,12 @@ Options
         label 'Exercise,Difficulty' \
         then stats1 -a 'count,median,mean,stddev' -f Difficulty -g Exercise \
         then format-values -f '%.3f'\
-        then sort -n Difficulty_median -f Exercise
+        then sort -n Difficulty_median -f Exercise \
+    | if set -q _flag_all
+        cat
+    else
+        awk '$2 > 1'
+    end
+
     popd
 end
