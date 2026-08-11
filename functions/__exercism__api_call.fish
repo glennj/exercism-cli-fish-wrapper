c543e3fa6f5959a19fc2a5b366207d52c8f47890
Glenn Jackman <glenn.jackman@gmail.com>
2026-08-11 11:11:23 -0400

    abort on unsuccessful API call

--- functions/__exercism__api_call.fish
+++ functions/__exercism__api_call.fish
@@ -2,7 +2,7 @@ function __exercism__api_call
     # The _last_ argument will be the URI added to the exercism API root.
     # Anything before that will be taken as a curl option.
     # Example:
-    #   __exercism__api_call tracks/javascript/trophies
+    #   set result(__exercism__api_call tracks/javascript/trophies); or return 1
     #   __exercism__api_call -v --output hello_world.export.zip tracks/javascript/exercises/hello-world/export_solutions
 
     argparse --ignore-unknown 'v/verbose' -- $argv
@@ -14,8 +14,8 @@ function __exercism__api_call
     set Headers -H (__exercism__api_auth_header) \
                 -H "User-Agent: exercism_cli_fish_wrapper/0.1"
 
-    set curl_opts --silent --include
-    set -q _flag_verbose; and set curl_opts --verbose
+    set curl_opts --silent --include --location --write-out '\n%{response_code}'
+    set -q _flag_verbose; and set curl_opts $curl_opts --verbose
     set curl_opts $curl_opts $argv
 
     if set -q _flag_verbose
@@ -28,7 +28,24 @@ function __exercism__api_call
     set seen_blank 0
     set header
     set body
-    curl $Headers $curl_opts $uri | begin
+
+    set output (curl $Headers $curl_opts $uri)
+    set curl_exit_status $status
+
+    set response_code $output[-1]
+    set -e output[-1]
+    if test $response_code -eq 0
+        echo "Error: curl timed out waiting for $uri" >&2
+        return 4
+    else if test $response_code -eq 429
+        echo "Error 429: take a break for a while." >&2
+        return 1
+    else if test $response_code -ge 400
+        echo "Error $response_codeb: cannot fetch $uri" >&2
+        return 2
+    end
+    
+    printf '%s\n' $output | begin
         read x http_status x
         while read line
             set line (string replace --regex '\r$' "" $line)
@@ -42,10 +59,6 @@ function __exercism__api_call
         end
     end
 
-    if test $http_status -eq 429
-        echo "Error 429: take a break for a while." >&2
-        return 1
-    end
     string collect $body
 end
 
